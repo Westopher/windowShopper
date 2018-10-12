8b00a8fc5597a999b576dec1ef06b92c7e3f8fd8
West Kraemer <kraemerwest@gmail.com>
2018-10-12 18:37:36 -0400

    first commit

---
+++ windowShopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  windowShopper
+//
+//  Created by West Kraemer on 10/12/18.
+//  Copyright © 2018 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
