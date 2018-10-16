3995de86c9bd3b81bc2633d19bf4c4f19feba4e1
West Kraemer <kraemerwest@gmail.com>
2018-10-16 19:01:57 -0400

    add customized views and prepare them in currency text field views so that IB matches simulator

--- windowShopper/Views/CurrencyTxtField.swift
+++ windowShopper/Views/CurrencyTxtField.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
+//to ensure IB reflects the actual look of the app, first add IBDesignable, then add prep for interfacebuilder func, with the customizedview func you created (containing your customized views...). Finally, you want to call these views in your awake from nib func.
+@IBDesignable
+
 class CurrencyTxtField: UITextField {
 
+    
+    override func prepareForInterfaceBuilder() {
+        customizedView()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.25)
-        layer.cornerRadius = 5.0
-        textAlignment = .center
+        customizedView()
+        }
         
-        if let p = placeholder {
-            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
-            attributedPlaceholder = place
-            textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        func customizedView {
+            backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.25)
+            layer.cornerRadius = 5.0
+            textAlignment = .center
+            
+            if let p = placeholder {
+                let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+                attributedPlaceholder = place
+                textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         }
     }
 
